c5ee448186998c819f4c0af1208f3190a5ac8edc
ZathrasXI <janpatekphillips@gmail.com>
2022-02-14 19:56:01 +0000

    server is reloading when nunjucks template is changed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
+  @Render('message')
   root() {
     return { message: 'Hello world!' };
   }
--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,30 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
-//import * as nunjucks from 'nunjucks';
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  //const express = app.getHttpAdapter().getInstance();
+  const express = app.getHttpAdapter().getInstance();
 
-  //const views = join(__dirname, '..', 'views');
+  const nunjucksOptions: nunjucks.ConfigureOptions = {
+    autoescape: true,
+    throwOnUndefined: false,
+    trimBlocks: false,
+    lstripBlocks: false,
+    watch: true,
+    noCache: process.env.NODE_ENV === 'local' ? true : false,
+    express: app,
+  };
+
+  const views = join(__dirname, 'views');
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  //app.setBaseViewsDir(views);
-  app.setViewEngine('hbs');
-  //nunjucks.configure(views, { express });
-  //app.setViewEngine('njk');
+  app.setBaseViewsDir(views);
+  nunjucks.configure(views, { express });
+
+  const viewEnv = nunjucks.configure(views, nunjucksOptions);
+  app.engine('njk', viewEnv.render);
+  app.setViewEngine('njk');
 
   await app.listen(3000);
 }
